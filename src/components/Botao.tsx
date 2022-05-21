2b249724abdad0e5a552512a52fd33730c00c6af
vitinhom <104538433+vitinhom@users.noreply.github.com>
2022-05-20 21:20:39 -0300

    o formulario de criar item

--- src/components/Botao.tsx
+++ src/components/Botao.tsx
@@ -2,11 +2,12 @@ import { FunctionComponent } from "react";
 import "./Botao.css"
 type Props = {
     texto: String
+    clique?: ()  => void 
 }
 const Botao: FunctionComponent<Props> = function(props){
     return(
         <>
-        <button>{props.texto}</button>
+        <button onClick={props.clique}>{props.texto}</button>
         </>
     );
 };
--- src/pages/Itens.tsx
+++ src/pages/Itens.tsx
@@ -3,20 +3,34 @@ import Rodape from '../components/Rodape';
 import Cabecalho from '../components/Cabecalho';
 import './Itens.css'
 import Botao from '../components/Botao';
-const ItensPage = function(){
-        //let estado = 'INICIAL';
-        const [estado, setEstado] = useState('INICIAL');
+enum Estado {
+    Inicial,
+    Criar
+}
 
-        const botaoCriarClicado = function() {
-            //estado = 'CRIAR';
-            setEstado('CRIAR');
-        };
+const ItensPage = function() {
+    const [estado, setEstado] = useState(Estado.Inicial);
+
+    const botaoCriarClicado = function() {
+        //estado = 'CRIAR';
+        setEstado(Estado.Criar);
+    }
+
+    const botaoSalvarClicado = function() {
+        //estado = 'INICIAL';
+        setEstado(Estado.Inicial);  
+    };
+    const botaoCancelarClicado = function() {
+        //estado = 'INICIAL';
+        setEstado(Estado.Inicial);  
+    };
+
     return (
         <>
         <Cabecalho />
         
         <main>
-        <button onClick={botaoCriarClicado}>Teste</button>
+       
             <h1>Itens</h1>
             <ul>
                 <li>Item 1</li>
@@ -24,28 +38,27 @@ const ItensPage = function(){
                 <li>Item 3</li>
             </ul>
             
-            <Botao texto="Criar" />
-                {(estado === 'INICIAL') && (
-                    <Botao texto="Criar" />
-                )}
-                {(estado === 'CRIAR') && (
-            
-                        <form className='item'>
-                <label>
-                    Nome:
-                    <input type="text" placeholder="Nome" />
-                </label>
-                <label>
-                    Decrição:
-                    <textarea placeholder="Descrição"></textarea>
-                </label>
+            {(estado === Estado.Inicial) && (
+                <Botao texto="Criar" clique={botaoCriarClicado}/>
+            )}
+            {(estado === Estado.Criar) && (
+        
+                <form className='item'>
+                    <label>
+                        Nome:
+                        <input type="text" placeholder="Nome" />
+                    </label>
+                    <label>
+                        Decrição:
+                        <textarea placeholder="Descrição"></textarea>
+                    </label>
 
-                <div className='btns'>
-                    <Botao texto="Salvar" />
-                    <Botao texto="Cancelar" />
-                </div>
-            </form>)
-            }
+                    <div className='btns'>
+                        <Botao texto="Salvar" clique={botaoSalvarClicado} />
+                        <Botao texto="Cancelar" clique={botaoCancelarClicado} />
+                    </div>
+                </form>
+            )}
            
         </main>
             
